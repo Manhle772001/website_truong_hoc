f27a80794dce4d5f3884986fecbe9a631bdea014
lemanh <manhlaso2@gmail.com>
2024-04-02 08:31:10 +0700

    4/2/2024

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
 
 $( document ).ready(function() {
-    $('#open_search').on('click',()=>{
-       $('.sup_search').addClass('d-block')
+    var slideIndex = 0;
+    showSlide(slideIndex)
+
+    $('#open_search').on('click',(e)=>{
+    console.log($.contains($('.input_search')),$('#open_search'));
+       $('.sup_search').toggle()
     })
+
+
+    function showSlide(index){
+        var slides = $('.slide');
+        console.log(slides);
+
+        if(index >= slides.length) {slideIndex = 0}
+        if(index < 0) {slideIndex = slideIndex - 1}
+        slides.hide()
+        slides.eq(slideIndex).show();
+    }
 });
+
